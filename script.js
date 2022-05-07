0056b14e2e4fa02780fca0ef19d663855599fcfb
Tradewcs <67076169+Tradewcs@users.noreply.github.com>
2022-05-07 14:56:09 +0300

    123

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ let machineStrip = document.querySelector('.machine-strip');
 let machineHead = document.querySelector('.machine-head');
 
 for (let i = 0; i < 90; i++) {
-	let newCell = document.createElement('td');
-	newCell.id = `cell #${i}`;
-
-	newCell.innerHTML = i;
-
-
-	machineStrip.appendChild(newCell);
-
-	newCell.id = 'head ' + newCell.id;
-	machineHead.appendChild(newCell);
+	machineStrip.innerHTML += `<td id="cell#${i}"></td>`;
+	machineHead.innerHTML  += `<td class="td-invs" id="h#${i}"></td>`;
 }
 
-
-// document.getElementById('cell #5').innerHTML = 'a'
