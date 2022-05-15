2a63ef2a4499f38b706ee70988c5386864dfb5c5
Tradewcs <67076169+Tradewcs@users.noreply.github.com>
2022-05-15 20:10:39 +0300

    string work

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let alphabeth_arr = ['0'];
 let alphabeth_table = document.querySelector('.alphabet-symbols-choise');
 let machineStrip = document.querySelector('.machine-strip');
 let machineHead = document.querySelector('.machine-head');
 let machineState = document.querySelector('.machine-state');
 let nullSymbolChoice = document.getElementById('nullSymbolChoice');
-let addState = document.querySelector('.addState');
-let deleteState = document.querySelector('.deleteState');
+let addStateButton = document.querySelector('.addStateButton');
+let deleteStateButton = document.querySelector('.deleteStateButton');
 let programTable = document.querySelector('.machineProgram');
+let symbolChangePanel = document.querySelector('.symbolChangePanel');
+let stripSymbolSelect = document.getElementById('stripSymbolSelect');
 
-
-
+const cells_count = 10;
+let Alpabeth = ['0', '1'];
 let nullSymbol = '0';
 let HeadCurrentPosition = 0;
 let statesCount = 2;
 let currentState = 1;
+let selectedValueIndex;
 
 
-
-// addState.addEventListener('click', function() {
+// addStateButton.addEventListener('click', function() {
 	// 	ExternalAlphabet.innerHTML += 4;
 	
 	// 	// MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'machine-strip']);
@@ -27,11 +28,9 @@ let currentState = 1;
 	
 	
 	window.onload = () => {
-		fillStrip(100);
+		fillStrip(cells_count);
 		
-		
-		
-		setHead(3);
+		setHead(5);
 		
 		
 		// MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'machine-strip']);
@@ -45,18 +44,9 @@ function fillStrip(length) {
 	machineState.innerHTML = '';
 	
 	for (let i = 0; i < length; i++) {
-		machineStrip.innerHTML += `<td id="c#${i}">${nullSymbol}</td>`;
-		machineHead.innerHTML  += `<td id="h#${i}" class="td-invs"></td>`;
-		machineState.innerHTML += `<td id="s#${i}" class="td-invs"></td>`
-	}
-}
-	
-	
-function nullSymbolChoice_addOptions(symbols_arr) {
-	nullSymbolChoice.innerHTML = '';
-	
-	for (let i = 0; i < symbols_arr.length; i++) {
-		nullSymbolChoice.innerHTML += `<option>${symbols_arr[i]}</option>`;
+		machineStrip.innerHTML += `<td id="${i}c">${nullSymbol}</td>`;
+		machineHead.innerHTML  += `<td id="${i}h" class="td-invs"></td>`;
+		machineState.innerHTML += `<td id="${i}s" class="td-invs"></td>`
 	}
 }
 
@@ -66,15 +56,24 @@ alphabeth_table.onclick = function reloadNullSymbolChoice(event) {
 	
 	if (target.type == 'checkbox') {
 		if (target.checked) {
-			alphabeth_arr.push(target.name);
+			Alpabeth.push(target.name);
 		} else {
-			alphabeth_arr = alphabeth_arr.filter(element => element != target.name);
+			Alpabeth = Alpabeth.filter(element => element != target.name);
 		}
+
+		nullSymbol = nullSymbolChoice.value;
+		nullSymbolChoice_addOptions(Alpabeth);
+		stripSymbolSelect.innerHTML = nullSymbolChoice.innerHTML;
+		BuildProgramTable();
 	}
+}
+
+function nullSymbolChoice_addOptions(symbols_arr) {
+	nullSymbolChoice.innerHTML = '';
 	
-	nullSymbol = nullSymbolChoice.value;
-	nullSymbolChoice_addOptions(alphabeth_arr);
-	BuildProgramTable();
+	for (let i = 0; i < symbols_arr.length; i++) {
+		nullSymbolChoice.innerHTML += `<option>${symbols_arr[i]}</option>`;
+	}
 }
 
 nullSymbolChoice.onchange = function(event) {
@@ -83,31 +82,35 @@ nullSymbolChoice.onchange = function(event) {
 	setHead(HeadCurrentPosition);
 }
 
+
+
 function setHead(position) {
-	let cell = document.getElementById(`c#${HeadCurrentPosition}`);
-	let cellHead = document.getElementById(`h#${HeadCurrentPosition}`);
-	let cellState = document.getElementById(`s#${HeadCurrentPosition}`);
-	cellHead.innerHTML = '';
+	let cell      = document.getElementById(`${HeadCurrentPosition}c`);
+	let cellHead  = document.getElementById(`${HeadCurrentPosition}h`);
+	let cellState = document.getElementById(`${HeadCurrentPosition}s`);
 	cell.classList.remove('yellow');
+	cellHead.innerHTML = '';
+	cellState.innerHTML = '';
 	
 	HeadCurrentPosition = position;
 
-	cell = document.getElementById(`c#${HeadCurrentPosition}`);
-	cellHead = document.getElementById(`h#${HeadCurrentPosition}`);
-	cellState = document.getElementById(`s#${HeadCurrentPosition}`);
+	cell      = document.getElementById(`${HeadCurrentPosition}c`);
+	cellHead  = document.getElementById(`${HeadCurrentPosition}h`);
+	cellState = document.getElementById(`${HeadCurrentPosition}s`);
+
 	cell.classList.add('yellow');
 	cellHead.innerHTML = '↑';
-	
+	cellState.innerHTML = `q${currentState}`;
 }
 
-addState.onclick = () => {
+addStateButton.onclick = () => {
 	let statesList = document.querySelector('.statesList');
 	statesList.innerHTML += `<li>q${statesCount++}</li>`;
 
 	BuildProgramTable();
 }
 
-deleteState.onclick = () => {
+deleteStateButton.onclick = () => {
 	if (statesCount > 2) {
 		let statesList = document.querySelector('.statesList');
 		statesList.innerHTML = '';
@@ -116,30 +119,77 @@ deleteState.onclick = () => {
 		for (let i = 0; i < statesCount; i++) {
 			statesList.innerHTML += `<li>q${i}</li>`
 		}
-	}
 
-	
-	BuildProgramTable();
+
+		BuildProgramTable();
+	}
 }
 
 function BuildProgramTable() {
 	inner_str = '';
 	
-	alphabeth_arr.sort();
+	Alpabeth.sort();
 	
 	inner_str += '<tr> <td></td> ';
-	for (let i = 0; i < alphabeth_arr.length; i++) {
-		inner_str += `<td> ${alphabeth_arr[i]} </td>`;
+	for (let i = 0; i < Alpabeth.length; i++) {
+		inner_str += `<td> ${Alpabeth[i]} </td>`;
 	}
 	inner_str += '</tr>';
 	
 	for (let i = 0; i < statesCount; i++) {
 		inner_str += `<tr> <td> q${i} </td>`;
-		for (let j = 0; j < alphabeth_arr.length; j++) {
-			inner_str += '<td></td>';
+		for (let j = 0; j < Alpabeth.length; j++) {
+			inner_str += `<td id="${i}-${j}"></td>`;
 		}
 		inner_str += '</tr>';
 	}
 	
 	programTable.innerHTML = inner_str;
+}
+
+
+let id_previous_green_cell = '0c';
+machineStrip.onclick = (event) => {
+	symbolChangePanel.classList.remove('invisible');
+
+	
+	let currentCell = event.target;
+	if (!currentCell.classList.contains('green')) {
+		selectedValueIndex = 0;
+
+		document.getElementById(id_previous_green_cell).classList.remove('green');
+		currentCell.classList.add('green');
+		id_previous_green_cell = currentCell.id;
+
+		let stripSymbolSelect = document.getElementById('stripSymbolSelect');
+		
+		stripSymbolSelect.onchange = () => {
+			currentCell.innerHTML = stripSymbolSelect.value;
+		};
+
+	} else {
+		if (selectedValueIndex >= Alpabeth.length) {
+			selectedValueIndex = 0;
+		}
+		currentCell.innerHTML = Alpabeth[selectedValueIndex++];
+
+	}
+
+	stripSymbolSelect.value = currentCell.innerHTML;
+
+	let exitChoiseButton = document.getElementById('exitChoiseButton');
+	exitChoiseButton.onclick = () => {
+		currentCell.classList.remove('green');
+		symbolChangePanel.classList.add('invisible');
+	}
+
+	let setHeadButton = document.getElementById('setHeadButton');
+	setHeadButton.onclick = (event) => {
+		setHead(parseInt(currentCell.id));
+	}
+
+	let cleanStripButton = document.getElementById('cleanStripButton');
+	cleanStripButton.onclick = () => {
+		fillStrip(cells_count);
+	}
 }
